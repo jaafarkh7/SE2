b0843ad87506179553136bb1c2a756d463b4f5ff
jaafarkh7 <khatounjaafar@gmail.com>
2025-11-11 18:30:02 +0200

    Initial commit

---
+++ src/util/logger.ts
@@ -0,0 +1,38 @@
+import config from "../config";
+import winston from "winston";
+
+const {logDir, isDev} = config;
+
+const logFileFormat = winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json(),
+    winston.format.splat(),
+    winston.format.errors({ stack: true }),
+)
+
+const logConsoleFormat = winston.format.combine(
+    winston.format.colorize(),
+    winston.format.timestamp({ format:"HH:mm:ss" }),
+    winston.format.errors({ stack: true }),
+    winston.format.printf(({ timestamp, level, message, stack}) => {
+        return `[${timestamp}] ${level}: ${message} || ${stack || message}`;
+    }
+))
+
+const logger = winston.createLogger({
+    level: 'info',
+    transports: [
+        new winston.transports.File({ filename: "error.log", dirname: logDir, level: "error", format: logFileFormat }),
+        new winston.transports.File({ filename: "combined.log", dirname: logDir,  format: logFileFormat }),
+    ],
+    exceptionHandlers: [
+        new winston.transports.File({ filename: "exceptions.log", dirname: logDir })
+    ]
+});
+
+if (isDev) {
+    logger.add(new winston.transports.Console({format: logConsoleFormat}));
+    logger.level = "debug";
+}
+
+export default logger;
