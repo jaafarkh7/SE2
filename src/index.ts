3757bc9193d440fc872798ab12084fefee969c4f
jaafarkh7 <khatounjaafar@gmail.com>
2025-11-12 00:26:01 +0200

    Added json and xml files and tests

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,7 @@
-import { FinanceCalculator, ItemValidator, OrderManagment, PriceValidator,MaxPriceValidator, Validator} from "app";
+import logger from "./util/logger";
+import { readCsv } from "./util/parser";
 
-const orders = [
-  { id: 1, item: "Sponge", price: 15 },
-  { id: 2, item: "Chocolate", price: 20 },
-  { id: 3, item: "Fruit", price: 18 },
-  { id: 4, item: "Red Velvet", price: 25 },
-  { id: 5, item: "Coffee", price: 8 },
-];
-const rules = [
-        new ItemValidator(),
-        new PriceValidator(),
-        new MaxPriceValidator()
-];
-const orderManager = new OrderManagment(new Validator([]), new FinanceCalculator());
-for (const order of orders) {
-  orderManager.addOrder(order.item, order.price)
-}
-
-// Adding a new order directly
-const newItem = "Marble";
-const newPrice = 22;
-
-orderManager.addOrder(newItem, newPrice);
-
-
-console.log("Orders after adding a new order:", orderManager.getOrders());
-
-// Calculate Total Revenue directly
-console.log("Total Revenue:", orderManager.getTotalRevenue());
-
-// Calculate Average Buy Power directly
-console.log("Average Buy Power:", orderManager.getAverageBuyPower());
-
-// Fetching an order directly
-const fetchId = 2;
-const fetchedOrder = orderManager.getOrder(fetchId);
-console.log("Order with ID 2:", fetchedOrder);
-
-// Attempt to fetch a non-existent order
-const nonExistentId = 10;
-const nonExistentOrder = orderManager.getOrder(nonExistentId);
-console.log("Order with ID 10 (non-existent):", nonExistentOrder);
+async function main() {
+  const data = await readCsv("data/input.csv");
+  logger.info(data);
+  }
